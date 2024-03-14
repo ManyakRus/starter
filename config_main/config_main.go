00945479689acee66ebb9fabcdc7000e715bb8f9
Nikitin Aleksandr <nikitinav@atomsbt.ru>
2024-03-14 16:03:56 +0300

    сделал LoadENV_or_SettingsTXT()

--- config_main/config_main.go
+++ config_main/config_main.go
@@ -57,7 +57,13 @@ func LoadSettingsTxt() {
 
 	dir := micro.ProgramDir()
 	filename := dir + "settings.txt"
-	LoadEnv_from_file(filename)
+	FilenameShort := micro.LastWord(filename)
+	err := LoadEnv_from_file_err(filename)
+	if err != nil {
+		log.Debug("Can not parse ", FilenameShort, " file: ", filename, " warning: "+err.Error())
+	} else {
+		log.Info("load ", FilenameShort, " from file: ", filename)
+	}
 }
 
 // LoadSettingsTxt_err - загружает из файла settings.txt переменные в переменные окружения, возвращает ошибку
@@ -78,9 +84,9 @@ func LoadEnv_from_file(filename string) {
 
 	err := LoadEnv_from_file_err(filename)
 	if err != nil {
-		log.Debug("Can not parse "+FilenameShort+" file: ", filename, " warning: "+err.Error())
+		log.Debug("Can not parse ", FilenameShort, " file: ", filename, " warning: "+err.Error())
 	} else {
-		log.Info("load "+FilenameShort+" from file: ", filename)
+		log.Info("load ", FilenameShort, " from file: ", filename)
 	}
 }
 
@@ -95,12 +101,20 @@ func LoadEnv_from_file_err(filename string) error {
 
 // LoadENV_or_SettingsTXT - загружает из файла .env или settings.txt переменные в переменные окружения
 func LoadENV_or_SettingsTXT() {
+	//var errTXT error
+
+	//загрузим файл .env
 	errENV := LoadEnv_err()
-	var err2 error
-	if errENV != nil {
-		err2 = LoadSettingsTxt_err()
+	if errENV == nil {
+		return
 	}
-	if err2 != nil {
-		log.Panic("LoadENV_or_SettingsTXT() error: ", err2)
-	}
+
+	//загрузим settings.txt если нет файла .env
+	LoadSettingsTxt()
+	//errTXT = LoadSettingsTxt_err()
+	//if errTXT != nil {
+	//	log.Panic("LoadENV_or_SettingsTXT() error: ", errTXT)
+	//} else {
+	//	log.Info("LoadENV_or_SettingsTXT() ok")
+	//}
 }
