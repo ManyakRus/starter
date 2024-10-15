0e70f83959b6e8b987a54db0e36247a1411d77f3
Nikitin Aleksandr <nikitinav@atomsbt.ru>
2024-10-15 11:32:53 +0300

    сделал Show_Version()

--- make_version.sh
+++ make_version.sh
@@ -1,1 +1,7 @@
+# версия приложения из git заполняется в файл: version.txt
+# образец:
+# v1.0.4-23-gf3bbaf4 2024-10-14 14:43:55 +0300
+# v1.0.61 2024-10-14 14:25:20 +0300
+# git commit TAG + git commit HASH + git commit date and time
+
 echo $(git describe --tags $(git rev-parse HEAD)) $(git show --no-patch --format=%ci) >./pkg/version/version.txt
